4525b120db620945110275902e0ba0d5377cacca
Chris Wahl <github@wahlnetwork.com>
2014-07-22 13:49:53 -0500

    New RVTools script

---
+++ RVTools/query_rvtools_report.ps1
@@ -0,0 +1,112 @@
+﻿###################################################################
+# RVTools Queries
+###################################################################
+
+# Excel import function
+
+function Import-Excel
+{
+  param (
+    [string]$FileName,
+    [string]$WorksheetName,
+    [bool]$DisplayProgress = $true
+  )
+
+  if ($FileName -eq "") {
+    throw "Please provide path to the Excel file"
+    Exit
+  }
+
+  if (-not (Test-Path $FileName)) {
+    throw "Path '$FileName' does not exist."
+    exit
+  }
+
+  $FileName = Resolve-Path $FileName
+  $excel = New-Object -com "Excel.Application"
+  $excel.Visible = $false
+  $workbook = $excel.workbooks.open($FileName)
+
+  if (-not $WorksheetName) {
+    Write-Warning "Defaulting to the first worksheet in workbook."
+    $sheet = $workbook.ActiveSheet
+  } else {
+    $sheet = $workbook.Sheets.Item($WorksheetName)
+  }
+  
+  if (-not $sheet)
+  {
+    throw "Unable to open worksheet $WorksheetName"
+    exit
+  }
+  
+  $sheetName = $sheet.Name
+  $columns = $sheet.UsedRange.Columns.Count
+  $lines = $sheet.UsedRange.Rows.Count
+  
+  Write-Warning "Worksheet $sheetName contains $columns columns and $lines lines of data"
+  
+  $fields = @()
+  
+  for ($column = 1; $column -le $columns; $column ++) {
+    $fieldName = $sheet.Cells.Item.Invoke(1, $column).Value2
+    if ($fieldName -eq $null) {
+      $fieldName = "Column" + $column.ToString()
+    }
+    $fields += $fieldName
+  }
+  
+  $line = 2
+  
+  
+  for ($line = 2; $line -le $lines; $line ++) {
+    $values = New-Object object[] $columns
+    for ($column = 1; $column -le $columns; $column++) {
+      $values[$column - 1] = $sheet.Cells.Item.Invoke($line, $column).Value2
+    }  
+  
+    $row = New-Object psobject
+    $fields | foreach-object -begin {$i = 0} -process {
+      $row | Add-Member -MemberType noteproperty -Name $fields[$i] -Value $values[$i]; $i++
+    }
+    $row
+    $percents = [math]::round((($line/$lines) * 100), 0)
+    if ($DisplayProgress) {
+      Write-Progress -Activity:"Importing from Excel file $FileName" -Status:"Imported $line of total $lines lines ($percents%)" -PercentComplete:$percents
+    }
+  }
+  $workbook.Close()
+  $excel.Quit()
+}
+
+# Location of the spreadsheet
+	$xlspath = $null
+
+# Import tabs from RVTools
+	$tabvhost = import-excel -FileName $xlspath -WorksheetName "tabvHost"
+
+###################################################################
+# HOST Queries
+
+# Group the clusters together by the cluster name
+	$clusters = $tabvhost | Group {$_.Cluster}
+	
+	# Break down into individual clusters
+	foreach ($cluster in $clusters)
+		{
+		
+		Write-Host $cluster.Name 
+		[array]$entries = $cluster.Group
+		$PCPU = $null
+		$LCPU = $null
+		
+		# Look at each host in the cluster
+		foreach ($entry in $entries)
+			{
+			$PCPU += $entry.'# CPU' * $entry.'Cores per CPU'
+			$LCPU += $entry.'# CPU' * $entry.'# Cores'
+			}
+			
+		$PCPU
+		$LCPU
+		}
